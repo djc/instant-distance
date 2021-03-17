7fa279eebe3af6e8bfb1256ecb0dc681bcfed3fb
Dirkjan Ochtman <dirkjan@ochtman.nl>
2021-03-17 17:18:24 +0100

    Add some Python code to test bindings

---
+++ instant-distance-py/test/test.py
@@ -0,0 +1,14 @@
+import instant_distance, random
+
+def main():
+    points = [[random.random() for _ in range(300)] for _ in range(1024)]
+    config = instant_distance.Config()
+    (hnsw, ids) = instant_distance.Hnsw.build(points, config)
+    p = [random.random() for _ in range(300)]
+    search = instant_distance.Search()
+    hnsw.search(p, search)
+    for pid in search:
+        print(pid)
+
+if __name__ == '__main__':
+    main()
