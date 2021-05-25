f16d8351d56b1c611ab0507e2a55848749d8bef5
Nicholas Rempel <nick@lucent.is>
2021-05-25 15:42:51 -0700

    Update build_index to also build the index in-flight

--- instant-distance-py/examples/translations/build_index.py
+++ instant-distance-py/examples/translations/build_index.py
@@ -1,31 +1,39 @@
+import instant_distance
 import aiohttp
 import asyncio
 from progress.bar import IncrementalBar
+from progress.spinner import Spinner
+
 
 MAX_LINES = 100_000
+
 
 vector_paths = {
     "https://dl.fbaipublicfiles.com/fasttext/vectors-aligned/wiki.en.align.vec": "./data/wiki.en.align.trimmed.vec",
     "https://dl.fbaipublicfiles.com/fasttext/vectors-aligned/wiki.fr.align.vec": "./data/wiki.fr.align.trimmed.vec",
-    "https://dl.fbaipublicfiles.com/fasttext/vectors-aligned/wiki.it.align.vec": "./data/wiki.it.align.trimmed.vec"
+    "https://dl.fbaipublicfiles.com/fasttext/vectors-aligned/wiki.it.align.vec": "./data/wiki.it.align.trimmed.vec",
 }
 
-async def download():
-    print("Downloading vector files...")
+
+async def main():
+    id_config = instant_distance.Config()
+
+    print("Downloading vector files and building indexes...")
     async with aiohttp.ClientSession() as session:
-        for url, path in vector_paths.items():
-            lineno = 0
-            with IncrementalBar(path.split("/")[-1], max=MAX_LINES) as bar:
+        for lineno, (url, path) in enumerate(vector_paths.items()):
+            points = []
+            values = []
+            with IncrementalBar(f"Downloading {path}", max=MAX_LINES) as bar:
                 async with session.get(url) as resp:
-                    with open(path, 'w') as fd:
+                    with open(path, "w") as fd:
                         while True:
+                            lineno += 1
                             bar.next()
+
                             line = await resp.content.readline()
                             if not line:
                                 # EOF
                                 break
-
-                            lineno += 1
 
                             # We just use the top 100k embeddings to
                             # save on space and time
@@ -36,12 +44,22 @@ async def download():
                                 # 100,000 embeddings, 300 dimensions
                                 fd.write("100000 300\n")
                             else:
-                                fd.write(line.decode("utf-8"))
+                                linestr = line.decode("utf-8")
+                                tokens = linestr.split(" ")
 
-                            
-async def main():
-    await download()
-    
+                                # We track values here to build the instant distance index
+                                values.append(tokens[0])
+                                vec = tokens[1:]
+                                points.append([float(p) for p in vec])
+
+                                # Write out the data to our original .vec files
+                                fd.write(linestr)
+
+            # Build the instant-distance index and dump it out to a file with .idx suffix
+            print("Building index...")
+            hnsw = instant_distance.HnswMap.build(points, values, id_config)
+            hnsw.dump(path.replace(".vec", ".idx"))
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
